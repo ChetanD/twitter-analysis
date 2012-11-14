9ba57bc2333a9d4991d2529a10e5d87ccba1d7fc
Chetan <chetandhembre@gmail.com>
2012-11-14 21:25:18 +0530

    Update newnow.js

--- newnow.js
+++ newnow.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 var url=require('url');
 var path    =   require('path');
-<<<<<<< HEAD
-=======
+
 //this is server function to handle http request ..
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
+
 var server = require('http').createServer(function (req, res) {
 
     var pathname = url.parse(req.url).pathname;
@@ -13,34 +12,11 @@ var server = require('http').createServer(function (req, res) {
     console.log(pathname+":"+ext.length);
      if(ext.length==0)
      {
-<<<<<<< HEAD
-
-=======
-     	console.log("done!!!!");
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
         fs.readFile('./newnowclient.html', 'utf-8', function(error, content) {
             res.writeHead(200, {'Content-Type' : 'text/html'});
             res.end(content);
         });
     }
-<<<<<<< HEAD
-    else if(ext===".css"){
-    	console.log("done!!!! css:"+pathname+"\n");
-        fs.readFile( __dirname + pathname,function(err,data){
-	  	if(err){
-	  		console.log(err);
-	  		res.writeHead(500);
-	  		return res.end('error in loading...');
-	   	}
-
-        
-	   	res.writeHead(200, {'Content-Type' : 'text/css'});
-	   	res.end(data);
-	   	console.log(data);
-	  });
-    }
-=======
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
     else if(ext===".js"){
     	console.log("done!!!! js");
         fs.readFile(__dirname + pathname,function(error, content) {
@@ -48,42 +24,6 @@ var server = require('http').createServer(function (req, res) {
             res.end(content);
         });
     }
-<<<<<<< HEAD
-    else if(ext===".png"){
-    	console.log("done!!!! png");
-        fs.readFile( __dirname + pathname,function(err,data){
-		  	if(err){
-		  		console.log(err);
-		  		res.writeHead(500);
-		  		return res.end('error in loading...');
-		   	}
-
-        
-	   	res.writeHead(200, {'Content-Type' : 'image/png'});
-	   	res.end(data);
-	   	console.log(data);
-	  });
-    }
-    else if(ext===".gif"){
-    	console.log("done!!!! png");
-        fs.readFile( __dirname + pathname,function(err,data){
-		  	if(err){
-		  		console.log(err);
-		  		res.writeHead(500);
-		  		return res.end('error in loading...');
-		   	}
-
-        
-	   	res.writeHead(200, {'Content-Type' : 'image/gif'});
-	   	res.end(data);
-	   	console.log(data);
-	  });
-    }
-    
-});
-
-server.listen(8080);
-=======
     
 });
 
@@ -91,18 +31,13 @@ var  port=(process.env.PORT || 8080)
 server.listen(port);
 
 // import node.js lib like now.js and ntwiter
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
+
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
 
 var twitter = require('ntwitter');
 var credentials = require('./credentials.js');
 
-<<<<<<< HEAD
-
-=======
-//create new twiiter client
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
 var t = new twitter({
     consumer_key: credentials.consumer_key,
     consumer_secret: credentials.consumer_secret,
@@ -111,40 +46,29 @@ var t = new twitter({
 });
   
 
-<<<<<<< HEAD
-
-=======
-//this is function use to stream tweet
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
 function streamme(room){
   t.stream(
     'statuses/filter',
     { track: room },
     function(stream) {
         stream.on('data', function(data) {
-<<<<<<< HEAD
           if(data && data.user.profile_image_url){ 
 	           var newRoom=room.join(',');
 	           var obj=Object();
 	            obj.text=data.text;
 	            obj.pic=data.user.profile_image_url;
-=======
+
           if(data){ 
 	           var newRoom=room.join(',');
 	           var obj=Object();
 	            obj.text=data.text;
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
+
 	            if(data.coordinates && data.coordinates.coordinates){
 	            	obj.coordinates=data.coordinates.coordinates;
 	             } else if(data.place && data.place.bounding_box){
 			      var place = data.place.bounding_box.coordinates[0][0];
 			      	obj.coordinates=place;
 			     }
-<<<<<<< HEAD
-
-=======
-			   console.log(obj.coordinates);
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
 			   var tweet=JSON.stringify(obj);
 	           nowjs.getGroup(newRoom).now.receiveMessage("chetan", tweet);
          }              
@@ -153,14 +77,12 @@ function streamme(room){
 );	
 }
 
-<<<<<<< HEAD
-nowjs.on("connect", function(){
-
-=======
 
 nowjs.on("connect", function(){
-  console.log("Joined: " + this.now.name);
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
+
+
+
+nowjs.on("connect", function(){
   this.now.room="love,hate";
   
   nowjs.getGroup(this.now.room).addUser(this.user.clientId);
@@ -170,11 +92,6 @@ nowjs.on("connect", function(){
 });
 
 nowjs.on("disconnect", function(){
-<<<<<<< HEAD
-
-=======
-  console.log("Left: " + this.now.name);
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
 });
 
 
