c20ed6659f183f6f982ae3712718d02942ecfc9b
Chetan <chetandhembre@gmail.com>
2012-11-14 21:22:38 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,15 +1,10 @@
 var credentials = {
-<<<<<<< HEAD
-    consumer_key: '3KzwSHQ0txFoJ9At1wVOw',
-    consumer_secret: 'mmtPo3Y1XzkPiT1WhkaAQ8a0S0SadVfTLjrMReKVms',
-    access_token_key: '73310549-Ul3K4GDt6bnX8YxsYbb3gjuM9a1lII1bxjYofV5U',
-    access_token_secret: '3aaa8qU1avtwrtu2OmIYFjwuFPZCeawsnIIobI23Ts'
-=======
+
     consumer_key: 'XXXXXX',
     consumer_secret: 'XXXXXX',
     access_token_key: 'XXXXXX',
     access_token_secret: 'XXXXXX'
->>>>>>> 9ee464c37cda599039cd91ebde98024c6110d89f
+
 };
 
 module.exports = credentials;
